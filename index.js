bf86b9a45caaa0e80b6b88f54e98f2fc0ebd523b
ihsanabuhanifah <user@MacBook-Pro-user.local>
2021-12-13 17:34:29 +0700

    ubah port

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const port = process.env.APP_PORT || 6000;
 const key = process.env.KEY;
-const morgan = require('morgan')
+const morgan = require("morgan");
 require("dotenv").config();
-
- 
-  
-
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan(':user-agent'))
+app.use(morgan(":user-agent"));
 app.use(router);
 console.log(key);
-app.listen(process.env.APP_PORT ,() => {
+app.listen(process.env.APP_PORT || 3000, () => {
   console.log(`Server berjalan di port ${port}`);
 });
