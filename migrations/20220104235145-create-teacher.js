c5f3a948158f517f20e608a48c320ab35c5644c3
ihsanabuhanifah <user@MacBook-Pro-user.local>
2022-01-20 13:27:35 +0700

    update guru

--- migrations/20220104235145-create-teacher.js
+++ migrations/20220104235145-create-teacher.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      UserId: {
+      userId: {
         type: Sequelize.INTEGER,
         onDelete: "CASCADE",
         references: {
