61d171ab731682a78e77825a2a148b46bb4817e7
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2022-02-16 21:27:41 +0700

    git push

--- migrations/20220216141822-create-schedule-monitor.js
+++ migrations/20220216141822-create-schedule-monitor.js
@@ -17,11 +17,11 @@ module.exports = {
       kegiatan : {
         type :Sequelize.STRING
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
