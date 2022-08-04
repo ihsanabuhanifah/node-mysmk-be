20779188c5fe43cbad1c3fbea2ed0cbdc79f6b65
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2022-08-04 18:47:29 +0700

    buat api profile

--- controllers/Wali/ProfileController.js
+++ controllers/Wali/ProfileController.js
@@ -1,19 +1,62 @@
-const parentModel = require("../../models").Parent;
-const profile = async (req, res)=> {
-    res.json({
-        status : "ok"
-    })
-}
+const parentModel = require("../../models").parent;
+const models = require("../../models");
+const profile = async (req, res) => {
+  try {
+    const parent = await parentModel.findOne({
+      attributes: ["user_id", "hubungan"],
+      where: {
+        user_id: req.id,
+      },
+      include: [
+        {
+          model: models.student,
+          require: true,
+          as: "siswa",
+          include: [
+            {
+              model: models.kelas_student,
+              require : true,
+              as : "kelas_student",
+              attributes : ["id", "kelas_id",],
+              include : [
+                {
+                    model: models.kelas,
+                    require: true,
+                    as: "kelas",
+                    attributes : ['id', 'nama_kelas'],
+                }
+              ],
+              where : {
+                status : 1
+              }
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json({
+      parent,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({
+      msg: "Terjadi Kesalahan",
+    });
+  }
+  res.json({
+    status: "ok",
+  });
+};
 
 const create = async (req, res) => {
-    try{
+  try {
+  } catch (err) {
+    return res.status(403).json({
+      status: "fail",
+      msg: "Ada Kesalaan",
+    });
+  }
+};
 
-    }catch (err){
-        return res.status(403).json({
-            status : 'fail',
-            msg : 'Ada Kesalaan'
-        })
-    }
-}
-
-module.exports = { profile , create};
+module.exports = { profile, create };
