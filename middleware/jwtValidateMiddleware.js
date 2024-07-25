5b908eb839fc6497715a187b8c8c80e1b4c1b837
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2024-07-25 16:10:22 +0700

    fix authme

--- middleware/jwtValidateMiddleware.js
+++ middleware/jwtValidateMiddleware.js
@@ -1,42 +1,37 @@
 const JWT = require("jsonwebtoken");
-const dotenv = require("dotenv").config()
-const UserModel = require("../models").user
-const jwtValidateMiddleware =  (req, res, next) => {
+const dotenv = require("dotenv").config();
+const UserModel = require("../models").user;
+const jwtValidateMiddleware = (req, res, next) => {
   const bearerHeader = req.headers["x-authorization"];
-  if(!bearerHeader) return res.sendStatus(401)
+  if (!bearerHeader) return res.sendStatus(401);
   const bearerToken = bearerHeader.split(" ")[1];
-  JWT.verify(bearerToken, process.env.JWT_SECRET_ACCESS_TOKEN, async(err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        status: "fail",
-        message: "Invalid Token",
-        data: err,
-      });
-    } else {
+  JWT.verify(
+    bearerToken,
+    process.env.JWT_SECRET_ACCESS_TOKEN,
+    async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          status: "fail",
+          message: "Invalid Token",
+          data: err,
+        });
+      } else {
+        req.id = decoded.id;
+        req.email = decoded.email;
+        req.name = decoded.name;
+        req.role = decoded.role;
+        req.roleId = decoded.roleId;
+        req.StudentId = decoded?.StudentId;
+        (req.semesterAktif = decoded?.semesterAktif),
+          (req.tahunAjaranAktif = decoded?.tahunAjaranAktif),
+          (req.teacher_id = decoded?.teacher_id);
 
-      
+        req.allRole = decoded.allRole;
 
-      req.id = decoded.id;
-      req.email = decoded.email;
-      req.name = decoded.name
-      req.role = decoded.role
-      req.roleId = decoded.roleId
-      req.StudentId = decoded?.StudentId
-      req.semesterAktif = decoded?.semesterAktif,
-      req.tahunAjaranAktif = decoded?.tahunAjaranAktif,
-      req.teacher_id = decoded?.teacher_id
-
-      req.allRole = decoded.allRole
-      const user = await UserModel.findByPk(decoded.id)
-     if (user === null) return res.status(404).json({
-       status : "Fail",
-       message : "User tidak ditemukan"
-     })
-
-     
-      next();
+        next();
+      }
     }
-  });
+  );
 };
 
 module.exports = jwtValidateMiddleware;
