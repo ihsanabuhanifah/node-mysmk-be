e2ba3d8b7f8dfe5e64f0f542908b99a14fc5a575
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2024-08-22 09:13:22 +0700

    tambah nis pada wali santri

--- routers/Guru/index.js
+++ routers/Guru/index.js
@@ -150,9 +150,8 @@ const {
   submitByAdmin,
 } = require("../../controllers/Guru/KehadiranGuruController");
 const adminAccessMiddleware = require("../../middleware/adminAccessMiddleware");
-const { listPenilaianByTeacher, remidial } = require("../../controllers/Guru/NilaiController");
-const { getListWali, updateWali, detailWali } = require("../../controllers/Guru/WaliController");
-const { ListPembayaran, createKartuSpp, detailPembayaran, daftarSiswa, detailPembayaranSiswa, createNotification, updateResponse } = require("../../controllers/Wali/PembayaranController");
+const { getListWali, updateWali, detailWali, createBulkWali } = require("../../controllers/Guru/WaliController");
+const { ListPembayaran, createKartuSpp, updateAprroval, detailPembayaran, deleteKartu, daftarSiswa, detailPembayaranSiswa, createNotification, updateResponse } = require("../../controllers/Wali/PembayaranController");
 
 
 
